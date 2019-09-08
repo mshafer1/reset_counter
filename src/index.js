c357341537411fdde5b52b303069e6fa2cf1a082
Matthew <2565361+mshafer1@users.noreply.github.com>
2019-09-08 18:05:32 -0500

    got it working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function setCookie(cname, cvalue, exdays) {
+    // https://www.w3schools.com/js/js_cookies.asp
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+
+function getCookie(cname) {
+    // from https://www.w3schools.com/js/js_cookies.asp
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    console.log("Cookies: ", document.cookie)
+    var ca = decodedCookie.split(';');
+    console.log(ca);
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function reset() {
+    console.log("Resetting");
+    var now = new Date().toUTCString();
+    setCookie('reset', now);
+    window.location.reload(); 
+}
+
+class ResetButton extends React.Component {
+    render () {
+        return (
+        <button class="padLeft" onClick={() => reset()}>Reset</button>
+        )
+    }
+}
+
+class Counter extends React.Component {
+    render () {
+        var value = new Date(getCookie('reset'));
+        var now = new Date();
+        var elapsed = Math.abs(now.getTime() - value.getTime());
+        var diffDays = Math.floor(elapsed / (1000 * 60 * 60 * 24));
+        return (
+            <span class="padLeft">
+                {diffDays} Days
+            </span>
+        )
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <div class="sign">
+        <Counter value={0}/>
+        <p>Since someone reset this sign.</p>
+        < ResetButton />
+    </div>,
+    document.getElementById('root')
+);
